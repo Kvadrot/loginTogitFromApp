9efb072c4d5be6f7ebd0b329d79743c8df1f4acf
Kvadrot <send.me.your.maild@gmail.com>
2021-02-22 15:46:43 +0200

    correct sec, id

--- PM_hm6/hm6/hm6/Network/ClientConstants.swift
+++ PM_hm6/hm6/hm6/Network/ClientConstants.swift
@@ -9,8 +9,8 @@ import Foundation
 
 struct ClientConstants {
     
-    static let CLIENT_ID = "0e02dd9f5eb38c4211c6"
-    static let CLIENT_SECRET = "122890a19ea2a495835956b4688c41a26c36eea4"
+    static let CLIENT_ID = "201f606bccba555e10cb"
+    static let CLIENT_SECRET = "dbaec1e2b68e701c08bcdf1f68aac101892ded04"
     static let REDIRECT_URI = "https://github.com/kvadrot"
     static let SCOPE = "read:user,user:email"
     static let TOKENURL = "https://github.com/login/oauth/access_token"
